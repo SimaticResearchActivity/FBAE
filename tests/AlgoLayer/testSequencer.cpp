5a9ec21952923c0eb89abdf4281c0c8e85514b75
Michel SIMATIC <michel.simatic@telecom-sudparis.eu>
2024-02-18 08:17:08 +0100

    Improve variable names in testSequencer.cpp

--- tests/AlgoLayer/testSequencer.cpp
+++ tests/AlgoLayer/testSequencer.cpp
@@ -20,17 +20,17 @@ namespace fbae_test_Sequencer {
         vector<HostTuple> sites(nbSites, HostTuple{"", 0});
         Arguments arguments{sites};
         auto commLayer = make_unique<CommStub>();
-        auto comm = commLayer.get();
+        auto commLayerRaw= commLayer.get();
         auto algoLayer = make_unique<Sequencer>(std::move(commLayer));
-        auto algo = algoLayer.get();
+        auto algoLayerRaw= algoLayer.get();
         SessionStub sessionStub{arguments, sequencerRank, std::move(algoLayer)};
 
-        algo->execute();
+        algoLayerRaw->execute();
 
-        EXPECT_EQ(nbSites - 1, comm->getConnectedDest().size());
-        EXPECT_EQ(1, comm->getConnectedDest()[0]);
-        EXPECT_EQ(2, comm->getConnectedDest()[1]);
-        EXPECT_EQ(3, comm->getConnectedDest()[2]);
+        EXPECT_EQ(nbSites - 1, commLayerRaw->getConnectedDest().size());
+        EXPECT_EQ(1, commLayerRaw->getConnectedDest()[0]);
+        EXPECT_EQ(2, commLayerRaw->getConnectedDest()[1]);
+        EXPECT_EQ(3, commLayerRaw->getConnectedDest()[2]);
     }
 
     TEST(Sequencer, BroadcasterConnections) {
@@ -38,15 +38,15 @@ namespace fbae_test_Sequencer {
         vector<HostTuple> sites(nbSites, HostTuple{"", 0});
         Arguments arguments{sites};
         auto commLayer = make_unique<CommStub>();
-        auto comm = commLayer.get();
+        auto commLayerRaw= commLayer.get();
         auto algoLayer = make_unique<Sequencer>(std::move(commLayer));
-        auto algo = algoLayer.get();
+        auto algoLayerRaw= algoLayer.get();
         SessionStub sessionStub{arguments, sequencerRank + 1, std::move(algoLayer)};
 
-        algo->execute();
+        algoLayerRaw->execute();
 
-        EXPECT_EQ(1, comm->getConnectedDest().size());
-        EXPECT_EQ(sequencerRank, comm->getConnectedDest()[0]);
+        EXPECT_EQ(1, commLayerRaw->getConnectedDest().size());
+        EXPECT_EQ(sequencerRank, commLayerRaw->getConnectedDest()[0]);
     }
 
     TEST(Sequencer, TotalOrderBroadcast) {
@@ -54,23 +54,23 @@ namespace fbae_test_Sequencer {
         vector<HostTuple> sites(nbSites, HostTuple{"", 0});
         Arguments arguments{sites};
         auto commLayer = make_unique<CommStub>();
-        auto comm = commLayer.get();
+        auto commLayerRaw= commLayer.get();
         auto algoLayer = make_unique<Sequencer>(std::move(commLayer));
-        auto algo = algoLayer.get();
+        auto algoLayerRaw= algoLayer.get();
         SessionStub sessionStub{arguments, sequencerRank + 1, std::move(algoLayer)};
 
-        algo->execute();
+        algoLayerRaw->execute();
         constexpr auto payload{"A"};
         auto s {serializeStruct<SessionTest>(SessionTest{SessionMsgId::TestMessage,
                                                                     payload})};
-        algo->totalOrderBroadcast(std::move(s));
+        algoLayerRaw->totalOrderBroadcast(std::move(s));
 
         // Check that 1 message was sent
-        EXPECT_EQ(1, comm->getSent().size());
+        EXPECT_EQ(1, commLayerRaw->getSent().size());
         // Check that this message was sent to sequencer
-        EXPECT_EQ(sequencerRank, comm->getSent()[0].first);
+        EXPECT_EQ(sequencerRank, commLayerRaw->getSent()[0].first);
         // Check contents of this message
-        auto msgBroadcast{deserializeStruct<fbae_SequencerAlgoLayer::StructBroadcastMessage>(std::move(comm->getSent()[0].second))};
+        auto msgBroadcast{deserializeStruct<fbae_SequencerAlgoLayer::StructBroadcastMessage>(std::move(commLayerRaw->getSent()[0].second))};
         EXPECT_EQ(fbae_SequencerAlgoLayer::MsgId::BroadcastRequest, msgBroadcast.msgId);
         EXPECT_EQ(0, msgBroadcast.senderPos); // 0 because broadcaster with rank 1 has position 0 among broadcasters
         auto sessionMsg{deserializeStruct<SessionTest>(std::move(msgBroadcast.sessionMsg))};
@@ -83,12 +83,12 @@ namespace fbae_test_Sequencer {
         vector<HostTuple> sites(nbSites, HostTuple{"", 0});
         Arguments arguments{sites};
         auto commLayer = make_unique<CommStub>();
-        auto comm = commLayer.get();
+        auto commLayerRaw= commLayer.get();
         auto algoLayer = make_unique<Sequencer>(std::move(commLayer));
-        auto algo = algoLayer.get();
+        auto algoLayerRaw= algoLayer.get();
         SessionStub sessionStub{arguments, sequencerRank, std::move(algoLayer)};
 
-        algo->execute();
+        algoLayerRaw->execute();
         constexpr auto payload{"A"};
         constexpr rank_t senderPos = 42;
         auto s {serializeStruct<SessionTest>(SessionTest{SessionMsgId::TestMessage,
@@ -96,19 +96,19 @@ namespace fbae_test_Sequencer {
         auto msg {serializeStruct<fbae_SequencerAlgoLayer::StructBroadcastMessage>(fbae_SequencerAlgoLayer::StructBroadcastMessage{fbae_SequencerAlgoLayer::MsgId::BroadcastRequest,
                                                                                senderPos,
                                                                                std::move(s)})};
-        algo->callbackHandleMessage(std::move(msg));
+        algoLayerRaw->callbackHandleMessage(std::move(msg));
 
         // Check that 3 messages are sent by Sequencer
-        EXPECT_EQ(nbSites-1, comm->getSent().size());
+        EXPECT_EQ(nbSites-1, commLayerRaw->getSent().size());
         // Check that destinations of all of these messages
-        EXPECT_EQ(1, comm->getSent()[0].first);
-        EXPECT_EQ(2, comm->getSent()[1].first);
-        EXPECT_EQ(3, comm->getSent()[2].first);
+        EXPECT_EQ(1, commLayerRaw->getSent()[0].first);
+        EXPECT_EQ(2, commLayerRaw->getSent()[1].first);
+        EXPECT_EQ(3, commLayerRaw->getSent()[2].first);
         // Check second and third messages are equal to first message.
-        EXPECT_EQ(comm->getSent()[0].second, comm->getSent()[1].second);
-        EXPECT_EQ(comm->getSent()[0].second, comm->getSent()[2].second);
+        EXPECT_EQ(commLayerRaw->getSent()[0].second, commLayerRaw->getSent()[1].second);
+        EXPECT_EQ(commLayerRaw->getSent()[0].second, commLayerRaw->getSent()[2].second);
         // Check contents of the first message
-        auto msgBroadcast{deserializeStruct<fbae_SequencerAlgoLayer::StructBroadcastMessage>(std::move(comm->getSent()[0].second))};
+        auto msgBroadcast{deserializeStruct<fbae_SequencerAlgoLayer::StructBroadcastMessage>(std::move(commLayerRaw->getSent()[0].second))};
         EXPECT_EQ(fbae_SequencerAlgoLayer::MsgId::Broadcast, msgBroadcast.msgId);
         EXPECT_EQ(senderPos, msgBroadcast.senderPos);
         auto sessionMsg{deserializeStruct<SessionTest>(std::move(msgBroadcast.sessionMsg))};
@@ -122,10 +122,10 @@ namespace fbae_test_Sequencer {
         Arguments arguments{sites};
         auto commLayer = make_unique<CommStub>();
         auto algoLayer = make_unique<Sequencer>(std::move(commLayer));
-        auto algo = algoLayer.get();
+        auto algoLayerRaw= algoLayer.get();
         SessionStub sessionStub{arguments, sequencerRank + 1, std::move(algoLayer)};
 
-        algo->execute();
+        algoLayerRaw->execute();
         constexpr auto payload{"A"};
         constexpr rank_t senderPos = 42;
         auto s {serializeStruct<SessionTest>(SessionTest{SessionMsgId::TestMessage,
@@ -133,7 +133,7 @@ namespace fbae_test_Sequencer {
         auto msg {serializeStruct<fbae_SequencerAlgoLayer::StructBroadcastMessage>(fbae_SequencerAlgoLayer::StructBroadcastMessage{fbae_SequencerAlgoLayer::MsgId::Broadcast,
                                                                                                                                    senderPos,
                                                                                                                                    std::move(s)})};
-        algo->callbackHandleMessage(std::move(msg));
+        algoLayerRaw->callbackHandleMessage(std::move(msg));
 
         // Check that 1 message has been delivered
         EXPECT_EQ(1, sessionStub.getDelivered().size());
