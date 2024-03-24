1568c2d15219b04ca3249f252cb59c1a7c06a068
emile <emile@guyotfamily.com>
2024-03-24 23:57:19 +0100

    Added some comments in LCR.h to explain some things.

--- src/lib/AlgoLayer/LCR/LCR.h
+++ src/lib/AlgoLayer/LCR/LCR.h
@@ -4,6 +4,10 @@
 #include "LCRMessage.h"
 #include "LCRTypedefs.h"
 
+// Algorithm described in the following paper: https://infoscience.epfl.ch/record/149218/files/paper.pdf?version=2
+// Page 9 gives pseudocode for the algorithm.
+// Differences: messages do not contain the entire vector clock of the site that sent it,
+// only the value for its own rank.
 
 /** @brief The LCR Algorithm layer. */
 class LCR: public AlgoLayer {
