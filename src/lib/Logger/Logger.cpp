477a04585a2c92f6e043fda6f3cee4f698f6fa9b
emile <emile@guyotfamily.com>
2024-03-25 01:42:56 +0100

    Added Logger instance to streamline logging.

--- src/lib/Logger/Logger.cpp
+++ src/lib/Logger/Logger.cpp
@@ -1,36 +1,43 @@
 #include "Logger.h"
 
+#include <utility>
+
 Logger::Logger() = default;
 
-void Logger::logTrace(const std::string && caller, const std::string && message) {
+void Logger::logTrace(const std::string && caller, const std::string && message) const {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_INFO(loggerInternal, message);
 }
 
-void Logger::logInfo(const std::string && caller, const std::string && message) {
+void Logger::logInfo(const std::string && caller, const std::string && message) const {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_INFO(loggerInternal, message);
 }
 
-void Logger::logError(const std::string && caller, const std::string && message) {
+void Logger::logError(const std::string && caller, const std::string && message) const {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_ERROR(loggerInternal, message);
 }
 
-void Logger::logDebug(const std::string && caller, const std::string && message) {
+void Logger::logDebug(const std::string && caller, const std::string && message) const {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_DEBUG(loggerInternal, message);
 }
 
-void Logger::logWarn(const std::string && caller, const std::string && message) {
+void Logger::logWarn(const std::string && caller, const std::string && message) const {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_WARN(loggerInternal, message);
 }
 
-void Logger::logFatal(const std::string && caller, const std::string && message) {
+void Logger::logFatal(const std::string && caller, const std::string && message) const {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_FATAL(loggerInternal, message);
 }
+
+LoggerInstance Logger::instance(std::string caller) {
+    return LoggerInstance(std::move(caller));
+}
+
 
 static Logger logger;
 
@@ -39,4 +46,42 @@ void initializeLogger() {
 }
 const Logger &getLogger() {
     return logger;
+}
+
+LoggerInstance::LoggerInstance(std::string name) : instanceName(std::move(name)) {}
+
+void LoggerInstance::logTrace(const std::string && message) const {
+    getLogger().logTrace(
+            static_cast<const std::string &&>(instanceName),
+            static_cast<const std::string &&>(message));
+}
+
+void LoggerInstance::logInfo(const std::string && message) const {
+    getLogger().logInfo(
+            static_cast<const std::string &&>(instanceName),
+            static_cast<const std::string &&>(message));
+}
+
+void LoggerInstance::logError(const std::string && message) const {
+    getLogger().logError(
+            static_cast<const std::string &&>(instanceName),
+            static_cast<const std::string &&>(message));
+}
+
+void LoggerInstance::logDebug(const std::string && message) const {
+    getLogger().logDebug(
+            static_cast<const std::string &&>(instanceName),
+            static_cast<const std::string &&>(message));
+}
+
+void LoggerInstance::logWarn(const std::string && message) const {
+    getLogger().logWarn(
+            static_cast<const std::string &&>(instanceName),
+            static_cast<const std::string &&>(message));
+}
+
+void LoggerInstance::logFatal(const std::string && message) const {
+    getLogger().logFatal(
+            static_cast<const std::string &&>(instanceName),
+            static_cast<const std::string &&>(message));
 }
--- src/lib/Logger/Logger.h
+++ src/lib/Logger/Logger.h
@@ -2,17 +2,43 @@
 
 #include <string>
 
+class LoggerInstance;
+
 class Logger {
 public:
     explicit Logger();
 
-    void logTrace(const std::string && caller, const std::string && message);
-    void logInfo(const std::string && caller, const std::string && message);
-    void logError(const std::string && caller, const std::string && message);
-    void logDebug(const std::string && caller, const std::string && message);
-    void logWarn(const std::string && caller, const std::string && message);
-    void logFatal(const std::string && caller, const std::string && message);
+    static LoggerInstance instance(std::string caller);
+    void logTrace(const std::string && caller, const std::string && message) const;
+    void logInfo(const std::string && caller, const std::string && message) const;
+    void logError(const std::string && caller, const std::string && message) const;
+    void logDebug(const std::string && caller, const std::string && message) const;
+    void logWarn(const std::string && caller, const std::string && message) const;
+    void logFatal(const std::string && caller, const std::string && message) const;
 };
 
 void initializeLogger();
 const Logger &getLogger();
+
+//                          |
+//                          v annoying to call each time....
+// getLogger().logInfo("myFunction", "We are running my function!");
+
+class LoggerInstance {
+public:
+    explicit LoggerInstance(std::string name);
+
+    void logTrace(const std::string && message) const;
+    void logInfo(const std::string && message) const;
+    void logError(const std::string && message) const;
+    void logDebug(const std::string && message) const;
+    void logWarn(const std::string && message) const;
+    void logFatal(const std::string && message) const;
+private:
+    std::string instanceName;
+};
+
+
+// auto logger = getLogger().instance("myFunction");
+// logger.logInfo("We are running my function!");
+
