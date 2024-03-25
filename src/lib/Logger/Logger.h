a676f97a98d27e6b5f8dfe9044846756db3adcac
emile <emile@guyotfamily.com>
2024-03-25 01:51:20 +0100

    Added [[maybe_unused]] to some functions of LoggerInstance to suppress CLion warnings.

--- src/lib/Logger/Logger.h
+++ src/lib/Logger/Logger.h
@@ -9,6 +9,7 @@ public:
     explicit Logger();
 
     static LoggerInstance instance(std::string caller);
+
     void logTrace(const std::string && caller, const std::string && message) const;
     void logInfo(const std::string && caller, const std::string && message) const;
     void logError(const std::string && caller, const std::string && message) const;
@@ -28,12 +29,12 @@ class LoggerInstance {
 public:
     explicit LoggerInstance(std::string callerName);
 
-    void logTrace(const std::string && message) const;
-    void logInfo(const std::string && message) const;
-    void logError(const std::string && message) const;
-    void logDebug(const std::string && message) const;
-    void logWarn(const std::string && message) const;
-    void logFatal(const std::string && message) const;
+    [[maybe_unused]] void logTrace(const std::string && message) const;
+    [[maybe_unused]] void logInfo(const std::string && message) const;
+    [[maybe_unused]] void logError(const std::string && message) const;
+    [[maybe_unused]] void logDebug(const std::string && message) const;
+    [[maybe_unused]] void logWarn(const std::string && message) const;
+    [[maybe_unused]] void logFatal(const std::string && message) const;
 private:
     std::string callerName;
 };
