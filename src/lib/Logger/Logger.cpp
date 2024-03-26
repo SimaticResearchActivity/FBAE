06294548d4152fdef9083921736d36ccbeb21767
emile <emile@guyotfamily.com>
2024-03-26 17:17:08 +0100

    Reworked the logger to work in a more intelligent way.

--- src/lib/Logger/Logger.cpp
+++ src/lib/Logger/Logger.cpp
@@ -3,93 +3,91 @@
 #include <utility>
 #include <sstream>
 
-Logger::Logger() = default;
-
-inline void Logger::logTrace(const std::string && caller, const std::string && message) const {
+inline void Logger::trace(const std::string && caller, const std::string && message) noexcept {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_TRACE(loggerInternal, message);
 }
 
-inline void Logger::logInfo(const std::string && caller, const std::string && message) const {
+inline void Logger::info(const std::string && caller, const std::string && message) noexcept {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_INFO(loggerInternal, message);
 }
 
-inline void Logger::logError(const std::string && caller, const std::string && message) const {
+inline void Logger::error(const std::string && caller, const std::string && message) noexcept {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_ERROR(loggerInternal, message);
 }
 
-inline void Logger::logDebug(const std::string && caller, const std::string && message) const {
+inline void Logger::debug(const std::string && caller, const std::string && message) noexcept {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_DEBUG(loggerInternal, message);
 }
 
-inline void Logger::logWarn(const std::string && caller, const std::string && message) const {
+inline void Logger::warn(const std::string && caller, const std::string && message) noexcept {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_WARN(loggerInternal, message);
 }
 
-inline void Logger::logFatal(const std::string && caller, const std::string && message) const {
+inline void Logger::fatal(const std::string && caller, const std::string && message) noexcept {
     // auto loggerInternal = log4cxx::Logger::getLogger(caller);
     // LOG4CXX_FATAL(loggerInternal, message);
 }
 
-[[maybe_unused]] inline LoggerInstance Logger::instance(std::string caller) {
+[[maybe_unused]] inline LoggerInstance Logger::instance(std::string caller) noexcept {
     return LoggerInstance(std::move(caller));
 }
 
-[[maybe_unused]] inline LoggerInstance Logger::instanceOnSite(rank_t rank, const std::string &caller) {
+[[maybe_unused]] inline LoggerInstance Logger::instanceOnSite(rank_t rank, const std::string &caller) noexcept {
     std::stringstream stream;
     stream << "Rank " << std::to_string(static_cast<uint32_t>(rank)) <<  ": " << caller;
     return LoggerInstance(stream.str());
 }
 
 
-static Logger logger = Logger();
+//static Logger logger = Logger();
 
 //inline void initializeLogger() {
 //    logger = Logger();
 //}
-inline const Logger &getLogger() {
-    return logger;
-}
+//inline const Logger &getLogger() {
+//    return logger;
+//}
 
 inline LoggerInstance::LoggerInstance(std::string callerName) : callerName(std::move(callerName)) {}
 
 
-[[maybe_unused]] inline void LoggerInstance::logTrace(const std::string && message) const {
-    getLogger().logTrace(
+[[maybe_unused]] inline void LoggerInstance::trace(const std::string && message) const {
+    Logger::trace(
             static_cast<const std::string &&>(callerName),
             static_cast<const std::string &&>(message));
 }
 
-[[maybe_unused]] inline void LoggerInstance::logInfo(const std::string && message) const {
-    getLogger().logInfo(
+[[maybe_unused]] inline void LoggerInstance::info(const std::string && message) const {
+    Logger::info(
             static_cast<const std::string &&>(callerName),
             static_cast<const std::string &&>(message));
 }
 
-[[maybe_unused]] inline void LoggerInstance::logError(const std::string && message) const {
-    getLogger().logError(
+[[maybe_unused]] inline void LoggerInstance::error(const std::string && message) const {
+    Logger::error(
             static_cast<const std::string &&>(callerName),
             static_cast<const std::string &&>(message));
 }
 
-[[maybe_unused]] inline void LoggerInstance::logDebug(const std::string && message) const {
-    getLogger().logDebug(
+[[maybe_unused]] inline void LoggerInstance::debug(const std::string && message) const {
+    Logger::debug(
             static_cast<const std::string &&>(callerName),
             static_cast<const std::string &&>(message));
 }
 
-[[maybe_unused]] inline void LoggerInstance::logWarn(const std::string && message) const {
-    getLogger().logWarn(
+[[maybe_unused]] inline void LoggerInstance::warn(const std::string && message) const {
+    Logger::warn(
             static_cast<const std::string &&>(callerName),
             static_cast<const std::string &&>(message));
 }
 
-[[maybe_unused]] inline void LoggerInstance::logFatal(const std::string && message) const {
-    getLogger().logFatal(
+[[maybe_unused]] inline void LoggerInstance::fatal(const std::string && message) const {
+    Logger::fatal(
             static_cast<const std::string &&>(callerName),
             static_cast<const std::string &&>(message));
 }
--- src/lib/Logger/Logger.h
+++ src/lib/Logger/Logger.h
@@ -5,145 +5,27 @@
 #include <string>
 
 // How to use the logger:
-// We either call the getter and call one of the available log methods
+// We either call the actual logging functions
 // by passing in the caller (usually just the function) as well as the
 // message:
 //
-// getLogger().logInfo("myFunction", "We are running my function!");
+// Logger::info("myFunction", "We are running my function!");
 //
-//
-// Or we can bind the caller to a intermediate object called a LoggerInstance,
+// Or we can bind the caller to an intermediate object called a LoggerInstance,
 // and call these functions onto this state directly to avoid typing the caller
 // multiple times:
 //
-// auto logger = getLogger().instance("myFunction");
-// logger.logInfo("We are running my function!");
-// logger.logInfo("Here too!");
+// auto logger = Logger::instance("myFunction");
+// logger.info("We are running my function!");
+// logger.info("Here too!");
 //
 // When dealing with sites, we could also bind the site's rank to the caller
 // using the following function:
 //
-// auto logger = getLogger().instanceOnSite(myRank, "MyFunction");
-// logger.logInfo("We are running my function on my rank!");
+// auto logger = Logger::instanceOnSite(myRank, "MyFunction");
+// logger.info("We are running my function on my rank!");
 //
-// The list of all the logging types are: logTrace, logInfo, logError, logDebug, logWarn and logFatal.
-
-class LoggerInstance;
-
-/**
- * @brief A logger used to display a variety of information to the
- * end user.
- */
-class Logger {
-public:
-    explicit Logger();
-
-    /**
-     * @brief Instantiate a logger instance bound to a caller.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @return the instance of the logger bound to the caller.
-     *
-     * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logInfo("My function was called.")
-     */
-    [[maybe_unused]] static LoggerInstance instance(std::string caller);
-
-    /**
-     * @brief Instantiate a logger instance bound to a caller of a particular site.
-
-     * @param rank[in]: the rank of the site that initiated the log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @return the instance of the logger bound to the caller and it's site.
-     *
-     * @example
-     * auto logger = getLogger().instanceOnSite(myRank, "myFunction");
-     * logger.logInfo("My function was called.");
-     */
-    [[maybe_unused]] static LoggerInstance instanceOnSite(rank_t rank, const std::string &caller);
-
-    /** @brief Log a trace log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @param message[in]: the message we want to log.
-     *
-     * @example
-     * getLogger().logTrace("myFunction", "This is my trace message.");
-     * */
-    void logTrace(const std::string && caller, const std::string && message) const;
-
-    /** @brief Log a info log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @param message[in]: the message we want to log.
-     *
-     * @example
-     * getLogger().logInfo("myFunction", "This is my info message.");
-     * */
-    void logInfo(const std::string && caller, const std::string && message) const;
-
-    /** @brief Log a error log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @param message[in]: the message we want to log.
-     *
-     * @example
-     * getLogger().logError("myFunction", "This is my error message.");
-     * */
-    void logError(const std::string && caller, const std::string && message) const;
-
-    /** @brief Log a debug log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @param message[in]: the message we want to log.
-     *
-     * @example
-     * getLogger().logDebug("myFunction", "This is my debug message.");
-     * */
-    void logDebug(const std::string && caller, const std::string && message) const;
-
-    /** @brief Log a warning log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @param message[in]: the message we want to log.
-     *
-     * @example
-     * getLogger().logWarn("myFunction", "This is my warning message.");
-     * */
-    void logWarn(const std::string && caller, const std::string && message) const;
-
-    /** @brief Log a fatal log.
-     *
-     * @param caller[in]: the function that initiated the log.
-     *
-     * @param message[in]: the message we want to log.
-     *
-     * @example
-     * getLogger().logFatal("myFunction", "This is my fatal message.");
-     * */
-    void logFatal(const std::string && caller, const std::string && message) const;
-};
-
-//void initializeLogger();
-
-/**
- * @brief Getter for the internal logger singleton.
- *
- * @return A constant reference to the logger singleton.
- *
- * @example
- * getLogger().logInfo("myFunction", "My function has been called!");
- */
-const Logger &getLogger();
+// The list of all the logging types are: trace, info, error, debug, warn and fatal.
 
 class LoggerInstance {
 public:
@@ -156,64 +38,277 @@ public:
      * @param message[in]: the message we want to log.
      *
      * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logTrace("This is my trace message.");
+     * auto logger = Logger::instance("myFunction");
+     * logger.trace("This is my trace message.");
      * */
-    [[maybe_unused]] void logTrace(const std::string && message) const;
+    [[maybe_unused]] void trace(const std::string && message) const;
 
     /** @brief Log a info log.
      *
      * @param message[in]: the message we want to log.
      *
      * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logInfo("This is my info message.");
+     * auto logger = Logger::instance("myFunction");
+     * logger.info("This is my info message.");
      * */
-    [[maybe_unused]] void logInfo(const std::string && message) const;
+    [[maybe_unused]] void info(const std::string && message) const;
 
     /** @brief Log a error log.
      *
      * @param message[in]: the message we want to log.
      *
      * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logError("This is my error message.");
+     * auto logger = Logger::instance("myFunction");
+     * logger.error("This is my error message.");
      * */
-    [[maybe_unused]] void logError(const std::string && message) const;
+    [[maybe_unused]] void error(const std::string && message) const;
 
     /** @brief Log a debug log.
      *
      * @param message[in]: the message we want to log.
      *
      * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logDebug("This is my debug message.");
+     * auto logger = Logger::instance("myFunction");
+     * logger.debug("This is my debug message.");
      * */
-    [[maybe_unused]] void logDebug(const std::string && message) const;
+    [[maybe_unused]] void debug(const std::string && message) const;
 
     /** @brief Log a warning log.
      *
      * @param message[in]: the message we want to log.
      *
      * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logWarn("This is my warning message.");
+     * auto logger = Logger::instance("myFunction");
+     * logger.warn("This is my warning message.");
      * */
-    [[maybe_unused]] void logWarn(const std::string && message) const;
+    [[maybe_unused]] void warn(const std::string && message) const;
 
     /** @brief Log a fatal log.
      *
      * @param message[in]: the message we want to log.
      *
      * @example
-     * auto logger = getLogger().instance("myFunction");
-     * logger.logFatal("This is my fatal message.");
+     * auto logger = Logger::instance("myFunction");
+     * logger.fatal("This is my fatal message.");
      * */
-    [[maybe_unused]] void logFatal(const std::string && message) const;
+    [[maybe_unused]] void fatal(const std::string && message) const;
 private:
     /**
      * @brief the name of the function who initiated the log.
      */
     std::string callerName;
 };
+
+/**
+ * @brief A logger used to display a variety of information to the
+ * end user.
+ */
+
+namespace Logger {
+    /**
+     * @brief Instantiate a logger instance bound to a caller.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @return the instance of the logger bound to the caller.
+     *
+     * @example
+     * auto logger = Logger::instance("myFunction");
+     * logger.info("My function was called.")
+     */
+    LoggerInstance instance(std::string caller) noexcept;
+
+    /**
+     * @brief Instantiate a logger instance bound to a caller of a particular site.
+
+     * @param rank[in]: the rank of the site that initiated the log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @return the instance of the logger bound to the caller and it's site.
+     *
+     * @example
+     * auto logger = Logger::instanceOnSite(myRank, "myFunction");
+     * logger.info("My function was called.");
+     */
+    [[maybe_unused]] LoggerInstance instanceOnSite(rank_t rank, const std::string &caller) noexcept;
+
+    /** @brief Log a trace log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @param message[in]: the message we want to log.
+     *
+     * @example
+     * Logger::trace("myFunction", "This is my trace message.");
+     * */
+    void trace(const std::string && caller, const std::string && message) noexcept;
+
+    /** @brief Log a info log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @param message[in]: the message we want to log.
+     *
+     * @example
+     * Logger::info("myFunction", "This is my info message.");
+     * */
+    void info(const std::string && caller, const std::string && message) noexcept;
+
+    /** @brief Log a error log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @param message[in]: the message we want to log.
+     *
+     * @example
+     * Logger::error("myFunction", "This is my error message.");
+     * */
+    void error(const std::string && caller, const std::string && message) noexcept;
+
+    /** @brief Log a Debug log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @param message[in]: the message we want to log.
+     *
+     * @example
+     * Logger::debug("myFunction", "This is my debug message.");
+     * */
+    void debug(const std::string && caller, const std::string && message) noexcept;
+
+    /** @brief Log a warning log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @param message[in]: the message we want to log.
+     *
+     * @example
+     * Logger::warn("myFunction", "This is my warning message.");
+     * */
+    void warn(const std::string && caller, const std::string && message) noexcept;
+
+    /** @brief Log a fatal log.
+     *
+     * @param caller[in]: the function that initiated the log.
+     *
+     * @param message[in]: the message we want to log.
+     *
+     * @example
+     * Logger::fatal("myFunction", "This is my fatal message.");
+     * */
+    void fatal(const std::string && caller, const std::string && message) noexcept;
+}
+
+
+//class Logger {
+//public:
+//    explicit Logger();
+//
+//    /**
+//     * @brief Instantiate a logger instance bound to a caller.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @return the instance of the logger bound to the caller.
+//     *
+//     * @example
+//     * auto logger = Logger::instance("myFunction");
+//     * logger.info("My function was called.")
+//     */
+//    [[maybe_unused]] static LoggerInstance instance(std::string caller);
+//
+//    /**
+//     * @brief Instantiate a logger instance bound to a caller of a particular site.
+//
+//     * @param rank[in]: the rank of the site that initiated the log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @return the instance of the logger bound to the caller and it's site.
+//     *
+//     * @example
+//     * auto logger = Logger::instanceOnSite(myRank, "myFunction");
+//     * logger.info("My function was called.");
+//     */
+//    [[maybe_unused]] static LoggerInstance instanceOnSite(rank_t rank, const std::string &caller);
+//
+//    /** @brief Log a trace log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @param message[in]: the message we want to log.
+//     *
+//     * @example
+//     * Logger::trace("myFunction", "This is my trace message.");
+//     * */
+//    void trace(const std::string && caller, const std::string && message) const;
+//
+//    /** @brief Log an info log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @param message[in]: the message we want to log.
+//     *
+//     * @example
+//     * Logger::info("myFunction", "This is my info message.");
+//     * */
+//    void info(const std::string && caller, const std::string && message) const;
+//
+//    /** @brief Log an error log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @param message[in]: the message we want to log.
+//     *
+//     * @example
+//     * Logger::error("myFunction", "This is my error message.");
+//     * */
+//    void error(const std::string && caller, const std::string && message) const;
+//
+//    /** @brief Log a debug log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @param message[in]: the message we want to log.
+//     *
+//     * @example
+//     * Logger::debug("myFunction", "This is my debug message.");
+//     * */
+//    void debug(const std::string && caller, const std::string && message) const;
+//
+//    /** @brief Log a warning log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @param message[in]: the message we want to log.
+//     *
+//     * @example
+//     * Logger::warn("myFunction", "This is my warning message.");
+//     * */
+//    void warn(const std::string && caller, const std::string && message) const;
+//
+//    /** @brief Log a fatal log.
+//     *
+//     * @param caller[in]: the function that initiated the log.
+//     *
+//     * @param message[in]: the message we want to log.
+//     *
+//     * @example
+//     * Logger::fatal("myFunction", "This is my fatal message.");
+//     * */
+//    void fatal(const std::string && caller, const std::string && message) const;
+//};
+//
+////void initializeLogger();
+//
+///**
+// * @brief Getter for the internal logger singleton.
+// *
+// * @return A constant reference to the logger singleton.
+// *
+// * @example
+// * Logger::info("myFunction", "My function has been called!");
+// */
+//const Logger &getLogger();
 
