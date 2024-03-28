dd418ca85b7ad0d4cc1db36dc8a2a28c4bf22e46
emile <emile@guyotfamily.com>
2024-03-28 23:23:22 +0100

    Changed comments to documentation for the logger explanation at the begin of the header file.

--- src/lib/Logger/Logger.h
+++ src/lib/Logger/Logger.h
@@ -4,28 +4,28 @@
 
 #include <string>
 
-// How to use the logger:
-// We either call the actual logging functions
-// by passing in the caller (usually just the function) as well as the
-// message:
-//
-// Logger::info("myFunction", "We are running my function!");
-//
-// Or we can bind the caller to an intermediate object called a LoggerInstance,
-// and call these functions onto this state directly to avoid typing the caller
-// multiple times:
-//
-// auto logger = Logger::instance("myFunction");
-// logger.info("We are running my function!");
-// logger.info("Here too!");
-//
-// When dealing with sites, we could also bind the site's rank to the caller
-// using the following function:
-//
-// auto logger = Logger::instanceOnSite(myRank, "MyFunction");
-// logger.info("We are running my function on my rank!");
-//
-// The list of all the logging types are: trace, info, error, debug, warn and fatal.
+/// How to use the logger:
+/// We either call the actual logging functions
+/// by passing in the caller (usually just the function) as well as the
+/// message:
+///
+/// Logger::info("myFunction", "We are running my function!");
+///
+/// Or we can bind the caller to an intermediate object called a LoggerInstance,
+/// and call these functions onto this state directly to avoid typing the caller
+/// multiple times:
+///
+/// const auto logger = Logger::instance("myFunction");
+/// logger.info("We are running my function!");
+/// logger.info("Here too!");
+///
+/// When dealing with sites, we could also bind the site's rank to the caller
+/// using the following function:
+///
+/// const auto logger = Logger::instanceOnSite(myRank, "MyFunction");
+/// logger.info("We are running my function on my rank!");
+///
+/// The list of all the logging types are: trace, info, error, debug, warn and fatal.
 
 class LoggerInstance {
 public:
