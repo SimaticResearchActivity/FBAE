d1296e3829363b489f659ac894286c86aba1f4c5
emile <emile@guyotfamily.com>
2024-04-30 13:13:09 +0200

    Documented the stream logger's functions.

--- src/lib/Logger/StreamLogger.h
+++ src/lib/Logger/StreamLogger.h
@@ -67,17 +67,128 @@ namespace Logger {
     public:
         explicit StreamInstanceBuilder(std::string callerName) noexcept;
 
+        /**
+         * @brief Initiate a trace log stream instance. We can push onto a log
+         * stream however we wish, and the log will terminate when we
+         * use Logger::endLog.
+         *
+         * @example:
+         * auto logger = Logger::instance("myFunction");
+         *
+         * logger.trace() \<\< "My message" \<\< Logger::endLog;
+         *
+         * @return the instance of the trace stream.
+         */
         [[maybe_unused]] StreamInstance trace();
+
+        /**
+         * @brief Initiate a info log stream instance. We can push onto a log
+         * stream however we wish, and the log will terminate when we
+         * use Logger::endLog.
+         *
+         * @example:
+         * auto logger = Logger::instance("myFunction");
+         *
+         * logger.info() \<\< "My message" \<\< Logger::endLog;
+         *
+         * @return the instance of the info stream.
+         */
         [[maybe_unused]] StreamInstance info();
+
+        /**
+         * @brief Initiate a warn log stream instance. We can push onto a log
+         * stream however we wish, and the log will terminate when we
+         * use Logger::endLog.
+         *
+         * @example:
+         * auto logger = Logger::instance("myFunction");
+         *
+         * logger.warn() \<\< "My message" \<\< Logger::endLog;
+         *
+         * @return the instance of the warn stream.
+         */
         [[maybe_unused]] StreamInstance warn();
+
+        /**
+         * @brief Initiate an error log stream instance. We can push onto a log
+         * stream however we wish, and the log will terminate when we
+         * use Logger::endLog.
+         *
+         * @example:
+         * auto logger = Logger::instance("myFunction");
+         *
+         * logger.error() \<\< "My message" \<\< Logger::endLog;
+         *
+         * @return the instance of the error stream.
+         */
         [[maybe_unused]] StreamInstance error();
+
+        /**
+         * @brief Initiate a fatal log stream instance. We can push onto a log
+         * stream however we wish, and the log will terminate when we
+         * use Logger::endLog.
+         *
+         * @example:
+         * auto logger = Logger::instance("myFunction");
+         *
+         * logger.fatal() \<\< "My message" \<\< Logger::endLog;
+         *
+         * @return the instance of the fatal stream.
+         */
         [[maybe_unused]] StreamInstance fatal();
+
+        /**
+         * @brief Initiate a debug log stream instance. We can push onto a log
+         * stream however we wish, and the log will terminate when we
+         * use Logger::endLog.
+         *
+         * @example:
+         * auto logger = Logger::instance("myFunction");
+         *
+         * logger.debug() \<\< "My message" \<\< Logger::endLog;
+         *
+         * @return the instance of the debug stream.
+         */
         [[maybe_unused]] StreamInstance debug();
+
     private:
         std::string callerName;
     };
 
+    /**
+     * @brief Instance a new logger instance based off of a caller's name.
+     *
+     * @param callerName [in]: the name of the function that called this function.
+     *
+     * @return A new logger instance (of type StreamInstanceBuilder)
+     *
+     * @example:
+     *
+     * auto logger = Logger::instance("myFunction");
+     *
+     * logger.info() \<\< "This is some info" \<\< Logger::endLog;
+     * logger.warn() \<\< "This is a warning" \<\< Logger::endLog;
+     * // ...
+     */
     [[maybe_unused]] StreamInstanceBuilder instance(std::string callerName) noexcept;
+
+    /**
+     * @brief Instance a new logger instance based off of a caller's name and the site's rank.
+     *
+     * @param callerName [in]: the name of the function that called this function.
+     *
+     * @param rank [in]: the rank of the site that called this function.
+     *
+     * @return A new logger instance (of type StreamInstanceBuilder)
+     *
+     * @example:
+     *
+     * auto logger = Logger::instanceOnSite("myFunction", 3);
+     *
+     * logger.info() \<\< "This is some info from rank 3" \<\< Logger::endLog;
+     * logger.warn() \<\< "This is a warning from rank 3" \<\< Logger::endLog;
+     * // ...
+     */
     [[maybe_unused]] StreamInstanceBuilder instanceOnSite(const std::string &callerName, rank_t rank) noexcept;
     /**
      * @brief The function we call to create a instance of a StreamInstance.
@@ -85,6 +196,10 @@ namespace Logger {
      * @param streamType [in] The type of the stream we wish to construct. See Logger::StreamType for more information.
      * @param callerName [in] The name of the function or piece of code who wishes to log information.
      * @return A instance of a StreamInstance corresponding to the parameters provided to the function.
+     *
+     * @example:
+     *
+     * Logger::instanceStream(Logger::StreamType::Info, "myFunction") << "Here's some info" << Logger::endLog;
      */
     StreamInstance instanceStream(StreamType streamType, const std::string &callerName);
 
