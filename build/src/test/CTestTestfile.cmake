36688453f1986ae42d3c88563eb77f6d26339852
Thomas SCHNEIDER <thomas.schneider@telecom-sudparis.eu>
2024-07-29 09:55:17 +0200

    Clean warning logs

--- build/src/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thomas/Documents/Travail/FBAE/src/test
-# Build directory: /home/thomas/Documents/Travail/FBAE/build/src/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/thomas/Documents/Travail/FBAE/build/src/test/tests[1]_include.cmake")
